06b373792a2f481a249de7732729e95ae468f792
“JulianySavazzi” <julianysaz@gmail.com>
2024-01-05 22:42:45 -0300

    step 1 for consuming pokeapi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,19 @@
-// const btnTeste = document.getElementById("btn-teste")
-       
-// btnTeste.addEventListener(
-//     "click", function(){
-//         console.log('cliquei no botao!')
-//     }
-// )
+//consumir api -> biblioteca fetch API
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+//nativa do JS, funciona nos browsers mais atuais
+
+const offset = 0
+const limit = 12
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+//a api funciona com IO
+//o fetch(url) faz um request e trara um promise da api (é uma promessa de um response)
+//a resposta do fetch nao é instantanea -> processamento assincrono (processamento paralelo, uma hora ele traz a resposta)
+//metodo then -> quando termina de executar/processar a requisicao, esse methodo de callback retorna a resposta do servidor
+//o fetch faz o processamento da requisiçao e a funcao de callback passada no metodo then traz a resposta do servidor
+
+fetch(url).then(
+    function(response){
+        console.log(response)
+    }
+)
