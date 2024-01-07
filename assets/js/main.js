abb36d3be83810b1a72405b074c1a68dba6a37b2
“JulianySavazzi” <julianysaz@gmail.com>
2024-01-06 21:12:08 -0300

    example for converting response body for JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,17 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 //a resposta do fetch nao é instantanea -> processamento assincrono (processamento paralelo, uma hora ele traz a resposta)
 //metodo then -> quando termina de executar/processar a requisicao, esse methodo de callback retorna a resposta do servidor
 //o fetch faz o processamento da requisiçao e a funcao de callback passada no metodo then traz a resposta do servidor
+//podemos encadear varios then em uma promisse
 
 fetch(url).then(
+    //por padrao o fetch usa o method GET para requisiçoes
     function(response){
+        //converter response body para JSON
+        response.json().then(
+            function (responseBody){
+                console.log(responseBody)
+            }
+        )
         console.log(response)
     }
 ).catch(function (error){
