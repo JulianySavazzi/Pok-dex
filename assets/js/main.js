53039430c66b6dd47fb9890d57063b6c0b1eb09c
“JulianySavazzi” <julianysaz@gmail.com>
2024-01-09 22:08:44 -0300

    listing pokemons by API response in HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,12 +16,42 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 //arrow function (=>) -> substitui a function para simplificar o codigo (muito usada em callbacks)
 //por padrao o fetch usa o method GET para requisiçoes
 
+//pegar os pokemons vindos da api, e adicionar no <ol> pelo id
+const pokeApiList = document.getElementById('pokeList')
+
 fetch(url)
 //encadeamento de then para callback, com arrow functions, simplificando o codigo
 //converter promisse -> response body para JSON
     .then((response) => response.json())
-    .then((responseBody) => console.log(responseBody))
+    .then((responseBody) => responseBody.results)
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            const element = pokemonList[i]
+            // console.log(convertPokemonToHtml(element))
+            //acessar janela -> window
+            //document -> acessar documento HTML atual 
+            //innerHTML -> concatenar o codigo html passado por string com a pagina
+            pokeApiList.innerHTML += convertPokemonToHtml(element)
+        }
+    })
     .catch((error) => console.error(error))
     .finally(() => console.log('Requisição concluída!'))
 
     
+//converter pokemon para HTML
+function convertPokemonToHtml(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://upload.wikimedia.org/wikipedia/en/2/28/Pokémon_Bulbasaur_art.png" 
+                alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
