380e579e98cf1cb9c59f68d566a65205ebcaa248
“JulianySavazzi” <julianysaz@gmail.com>
2024-01-17 12:06:18 -0300

    update style for pokemons by type

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,12 @@ const pokeApiList = document.getElementById('pokeList')
 //converter pokemon para HTML
 function convertPokemonToHtml(pokemon){
     return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
                 <img src="${pokemon.photo}"
                 alt="${pokemon.name}">
